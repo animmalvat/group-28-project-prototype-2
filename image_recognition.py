1f0c2dcab1ad5af2f707fcc852c33c29cf53a2ff
Anim Malvat <malvat.anim0@gmail.com>
2020-02-29 14:49:49 -0500

    add comments to make more sense

--- image_recognition.py
+++ image_recognition.py
@@ -1,7 +1,15 @@
-import PIL
-from PIL import Image
-import pytesseract
+import PIL 
+from PIL import Image #image module to manipulate image
+import pytesseract #api that helps us to convert the image to strings
 
+#below is the location of tesseract that is installed on the system
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-temp = pytesseract.image_to_string(Image.open('1.jpeg'), lang="eng")
+
+#image that is to be converted
+image = Image.open('1.jpeg');
+
+#simple method that uses an image to convert the image to string
+temp = pytesseract.image_to_string(image, lang="eng")
+
+#printing the data
 print(temp)
